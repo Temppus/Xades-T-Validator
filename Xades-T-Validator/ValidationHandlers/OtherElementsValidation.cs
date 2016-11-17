489f0d2e987085e0f038245e7f2b7b12d9c26858
Navarad <donkoster@gmail.com>
2016-11-17 18:51:48 +0100

    Added OtherElementsValidation class

---
+++ Xades-T-Validator/ValidationHandlers/OtherElementsValidation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xades_T_Validator.Attributes;
+using Xades_T_Validator.ValidationHandlers.Base;
+using Xades_T_Validator.Wrappers;
+
+namespace Xades_T_Validator.ValidationHandlers
+{
+    [XadesTValidator(ValidationTaskName: "Overenie ostatných elementov profilu XAdES_ZEP, ktoré prináležia do špecifikácie XML Signature")]
+    class OtherElementsValidation : BaseXadesTValidator
+    {
+        public OtherElementsValidation(IEnumerable<XMLDocumentWrapper> documentWrappers) : base(documentWrappers)
+        {
+        }
+    }
+}
