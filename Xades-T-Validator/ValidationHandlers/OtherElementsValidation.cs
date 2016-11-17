3937149bae6a475abb3eaa8f6ff85836b58e5f01
Navarad <donkoster@gmail.com>
2016-11-17 20:01:39 +0100

    ds:Signature validation

--- Xades-T-Validator/ValidationHandlers/OtherElementsValidation.cs
+++ Xades-T-Validator/ValidationHandlers/OtherElementsValidation.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 using Xades_T_Validator.Attributes;
 using Xades_T_Validator.ValidationHandlers.Base;
 using Xades_T_Validator.Wrappers;
@@ -12,8 +14,35 @@ namespace Xades_T_Validator.ValidationHandlers
     [XadesTValidator(ValidationTaskName: "Overenie ostatných elementov profilu XAdES_ZEP, ktoré prináležia do špecifikácie XML Signature")]
     class OtherElementsValidation : BaseXadesTValidator
     {
+        private const string xmlnsDs = "http://www.w3.org/2000/09/xmldsig#";
+
         public OtherElementsValidation(IEnumerable<XMLDocumentWrapper> documentWrappers) : base(documentWrappers)
         {
+        }
+
+        [XadesTValidationHandler(ExecutionOrder: 1, Description: "ds:Signature: musí mať Id atribút, musí mať špecifikovaný namespace xmlns:ds")]
+        public ValidationError ValidationHandler1(XMLDocumentWrapper docWrapper)
+        {
+            ValidationError validationError = new ValidationError(docWrapper.XmlName, null);
+            XmlDocument xmlDoc = docWrapper.XmlDoc;
+
+            try
+            {
+                if (xmlDoc.DocumentElement.Attributes["Id"] == null)
+                {
+                    validationError.ErrorMessage = GetErrorMessage(MethodBase.GetCurrentMethod());
+                }
+                if (xmlDoc.DocumentElement.Attributes["xmlns:ds"].Value != xmlnsDs)
+                {
+                    validationError.ErrorMessage = GetErrorMessage(MethodBase.GetCurrentMethod());
+                }
+            }
+            catch (Exception /*ex*/)
+            {
+                validationError.ErrorMessage = GetErrorMessage(MethodBase.GetCurrentMethod());
+            }
+
+            return validationError;
         }
     }
 }
