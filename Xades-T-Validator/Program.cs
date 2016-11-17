6d2fd0c7b2dd942946e2b449e8a3f9a5273487f6
Tempus <Ivan.Benovic2@gmail.com>
2016-11-17 15:15:02 +0100

    Add project files.

---
+++ Xades-T-Validator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xades_T_Validator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
