16fd5985663be1d2170679abf877a53878489a47
Navarad <donkoster@gmail.com>
2016-11-18 11:50:31 +0100

    Fixed method names, Validation, removed try catch blocks

--- Xades-T-Validator/ValidationHandlers/OtherElementsValidation.cs
+++ Xades-T-Validator/ValidationHandlers/OtherElementsValidation.cs
@@ -29,44 +29,32 @@ namespace Xades_T_Validator.ValidationHandlers
         #region Signature-SignatureValue-SignedInfo
 
         [XadesTValidationHandler(ExecutionOrder: 1, Description: "ds:Signature: musí mať Id atribút, musí mať špecifikovaný namespace xmlns:ds")]
-        public ValidationError ValidationHandler1(XMLDocumentWrapper docWrapper)
+        public ValidationError ValidateSignature(XMLDocumentWrapper docWrapper)
         {
             ValidationError validationError = new ValidationError(docWrapper.XmlName, null);
             XmlDocument xmlDoc = docWrapper.XmlDoc;
 
-            try
-            {
-                if (xmlDoc.DocumentElement.SelectSingleNode("//ds:Signature", GetNamespaceManager(xmlDoc)).Attributes["Id"] == null)
-                {
-                    validationError.ErrorMessage = GetErrorMessage(MethodBase.GetCurrentMethod());
-                }
-                if (xmlDoc.DocumentElement.SelectSingleNode("//ds:Signature", GetNamespaceManager(xmlDoc)).Attributes["xmlns:ds"].Value != xmlnsDs)
-                {
-                    validationError.ErrorMessage = GetErrorMessage(MethodBase.GetCurrentMethod());
-                }
-            }
-            catch (Exception /*ex*/)
+            if (xmlDoc.DocumentElement.SelectSingleNode("//ds:Signature", GetNamespaceManager(xmlDoc)).Attributes["Id"] == null)
             {
                 validationError.ErrorMessage = GetErrorMessage(MethodBase.GetCurrentMethod());
             }
+            if (xmlDoc.DocumentElement.SelectSingleNode("//ds:Signature", GetNamespaceManager(xmlDoc)).Attributes["xmlns:ds"].Value != xmlnsDs)
+            {
+                validationError.ErrorMessage = GetErrorMessage(MethodBase.GetCurrentMethod());
+            }
+
+
 
             return validationError;
         }
 
         [XadesTValidationHandler(ExecutionOrder: 2, Description: "ds:SignatureValue – musí mať Id atribút")]
-        public ValidationError ValidationHandler2(XMLDocumentWrapper docWrapper)
+        public ValidationError ValidateSignatureValue(XMLDocumentWrapper docWrapper)
         {
             ValidationError validationError = new ValidationError(docWrapper.XmlName, null);
             XmlDocument xmlDoc = docWrapper.XmlDoc;
 
-            try
-            {
-                if (xmlDoc.DocumentElement.SelectSingleNode("//ds:Signature/ds:SignatureValue", GetNamespaceManager(xmlDoc)).Attributes["Id"] == null)
-                {
-                    validationError.ErrorMessage = GetErrorMessage(MethodBase.GetCurrentMethod());
-                }
-            }
-            catch (Exception /*ex*/)
+            if (xmlDoc.DocumentElement.SelectSingleNode("//ds:Signature/ds:SignatureValue", GetNamespaceManager(xmlDoc)).Attributes["Id"] == null)
             {
                 validationError.ErrorMessage = GetErrorMessage(MethodBase.GetCurrentMethod());
             }
@@ -75,52 +63,50 @@ namespace Xades_T_Validator.ValidationHandlers
         }
 
         [XadesTValidationHandler(ExecutionOrder: 3, Description: "overenie existencie referencií v ds:SignedInfo a hodnôt atribútov Id a Type voči profilu XAdES_ZEP pre: ds:KeyInfo element, ds: SignatureProperties element, xades: SignedProperties element, všetky ostatné referencie v rámci ds: SignedInfo musia byť referenciami na ds: Manifest elementy")]
-        public ValidationError ValidationHandler3(XMLDocumentWrapper docWrapper)
+        public ValidationError ValidateReferences(XMLDocumentWrapper docWrapper)
         {
             ValidationError validationError = new ValidationError(docWrapper.XmlName, null);
             XmlDocument xmlDoc = docWrapper.XmlDoc;
 
-            try
+            XmlNodeList references = xmlDoc.DocumentElement.SelectNodes("//ds:Signature/ds:SignedInfo/ds:Reference", GetNamespaceManager(xmlDoc));
+            foreach (XmlElement xmlRef in references)
             {
-                XmlNodeList references = xmlDoc.DocumentElement.SelectNodes("//ds:Signature/ds:SignedInfo/ds:Reference", GetNamespaceManager(xmlDoc));
-                foreach(XmlElement xmlRef in references){
-                    string refType = xmlRef.Attributes["Type"].Value;
-
-                    string targetId = xmlRef.Attributes["URI"].Value.Substring(1);
-                    var targetElement = xmlDoc.DocumentElement.SelectSingleNode("//ds:Signature//*[@Id='" + targetId + "']", GetNamespaceManager(xmlDoc));
-                    if (targetElement == null)
-                    {
-                        validationError.ErrorMessage = GetErrorMessage(MethodBase.GetCurrentMethod());
-                    }
-
-                    bool foundElementType = false;
-                    foreach (KeyValuePair<string, string> entry in refElementsInfos)
-                    {
-                        if (entry.Key == targetElement.Name && entry.Value == refType)
-                        {
-                            foundElementType = true;
-                            break;
-                        }
-                    }
-                    if(foundElementType == false)
-                    {
-                        validationError.ErrorMessage = GetErrorMessage(MethodBase.GetCurrentMethod());
-                    }
-                }
-                if (xmlDoc.DocumentElement.SelectSingleNode("//ds:Signature/ds:SignedInfo/ds:Reference[@Type='http://www.w3.org/2000/09/xmldsig#Object']", GetNamespaceManager(xmlDoc)) == null)
-                {
-                    validationError.ErrorMessage = GetErrorMessage(MethodBase.GetCurrentMethod());
-                }
-                if (xmlDoc.DocumentElement.SelectSingleNode("//ds:Signature/ds:SignedInfo/ds:Reference[@Type='http://www.w3.org/2000/09/xmldsig#SignatureProperties']", GetNamespaceManager(xmlDoc)) == null)
-                {
-                    validationError.ErrorMessage = GetErrorMessage(MethodBase.GetCurrentMethod());
-                }
-                if (xmlDoc.DocumentElement.SelectSingleNode("//ds:Signature/ds:SignedInfo/ds:Reference[@Type='http://uri.etsi.org/01903#SignedProperties']", GetNamespaceManager(xmlDoc)) == null)
+                string refType = xmlRef.Attributes["Type"].Value;
+                string targetId = xmlRef.Attributes["URI"].Value.Substring(1);
+                var targetElement = xmlDoc.DocumentElement.SelectSingleNode("//ds:Signature//*[@Id='" + targetId + "']", GetNamespaceManager(xmlDoc));
+                KeyValuePair<string, string> referedElement = new KeyValuePair<string, string>(targetElement?.Name != null ? targetElement.Name : "", refType);
+                if (!refElementsInfos.Contains(referedElement))
                 {
                     validationError.ErrorMessage = GetErrorMessage(MethodBase.GetCurrentMethod());
                 }
             }
-            catch (Exception ex)
+            if (xmlDoc.DocumentElement.SelectSingleNode("//ds:Signature/ds:SignedInfo/ds:Reference[@Type='http://www.w3.org/2000/09/xmldsig#Object']", GetNamespaceManager(xmlDoc)) == null)
+            {
+                validationError.ErrorMessage = GetErrorMessage(MethodBase.GetCurrentMethod());
+            }
+            if (xmlDoc.DocumentElement.SelectSingleNode("//ds:Signature/ds:SignedInfo/ds:Reference[@Type='http://www.w3.org/2000/09/xmldsig#SignatureProperties']", GetNamespaceManager(xmlDoc)) == null)
+            {
+                validationError.ErrorMessage = GetErrorMessage(MethodBase.GetCurrentMethod());
+            }
+            if (xmlDoc.DocumentElement.SelectSingleNode("//ds:Signature/ds:SignedInfo/ds:Reference[@Type='http://uri.etsi.org/01903#SignedProperties']", GetNamespaceManager(xmlDoc)) == null)
+            {
+                validationError.ErrorMessage = GetErrorMessage(MethodBase.GetCurrentMethod());
+            }
+            return validationError;
+        }
+
+        [XadesTValidationHandler(
+            ExecutionOrder: 4, 
+            Description: "overenie obsahu ds:SignatureProperties:" + 
+                            "musí mať Id atribút," + 
+                            "musí obsahovať dva elementy ds: SignatureProperty pre xzep: SignatureVersion a xzep: ProductInfos," + 
+                            "obidva ds: SignatureProperty musia mať atribút Target nastavený na ds: Signature, -oba kontrola cez mriežku  #signatureid")]
+        public ValidationError ValidateSignatureProperties(XMLDocumentWrapper docWrapper)
+        {
+            ValidationError validationError = new ValidationError(docWrapper.XmlName, null);
+            XmlDocument xmlDoc = docWrapper.XmlDoc;
+
+            if (xmlDoc.DocumentElement.SelectSingleNode("//ds:Signature/ds:Object/ds:SignatureProperties", GetNamespaceManager(xmlDoc)).Attributes["Id"] == null)
             {
                 validationError.ErrorMessage = GetErrorMessage(MethodBase.GetCurrentMethod());
             }
