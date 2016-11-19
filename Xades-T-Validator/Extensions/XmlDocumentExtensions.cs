7d64896960853e078969a40bad6b6936cbc587ec
Tempus <Ivan.Benovic2@gmail.com>
2016-11-19 21:57:59 +0100

    Merge fix.

--- Xades-T-Validator/Extensions/XmlDocumentExtensions.cs
+++ Xades-T-Validator/Extensions/XmlDocumentExtensions.cs
@@ -30,5 +30,10 @@ namespace Xades_T_Validator.Extensions
         {
             return xmlDoc.DocumentElement.SelectNodes(xPath, xmlDoc.NameSpaceManager());
         }
+
+        public static string AtrValue(this XmlNode xmlNode, string atrName)
+        {
+            return xmlNode.Attributes[atrName]?.Value;
+        }
     }
 }
